876ac08480519bf48d529f28119b439937f46979
Nur Aziz <sukabaru09@gmail.com>
2024-10-24 01:50:43 +0700

    create orders table migrations

---
+++ database/migrations/2024_10_24_014536_create_custom_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('custom_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders', 'id')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('url');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->integer('estimated_price');
+            $table->integer('total_price');
+            $table->boolean('is_available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('custom_order_items');
+    }
+};
