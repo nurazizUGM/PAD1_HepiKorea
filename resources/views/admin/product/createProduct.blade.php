3486520075d0eea67254c3e0a55d46b0215b37aa
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-10-25 13:20:08 +0700

    form create product admin

---
+++ resources/views/admin/product/createProduct.blade.php
@@ -0,0 +1,192 @@
+@extends('layout.admin')
+
+@section('title', 'Product')
+
+@section('content')
+
+    <div class="bg-[#EFEFEF] border-gray-200 rounded-lg">
+        <!-- Tabs (product, category, carousel) -->
+        <div class="mb-3">
+            <ul class="flex flex-wrap -mb-px text-lg font-bold text-center text-black gap-x-16" id="default-tab"
+                data-tabs-toggle="#default-tab-content"
+                data-tabs-active-classes="text-black hover:text-black dark:text-purple-500 dark:hover:text-purple-500 border-b-4 border-orange-400 hover:border-orange-400 dark:border-purple-500"
+                data-tabs-inactive-classes="dark:border-transparent text-black hover:text-orange-400 dark:text-gray-400 border-transparent hover:border-transparent dark:border-gray-700 dark:hover:text-gray-300"
+                role="tablist">
+                <!-- Tab product -->
+                <li class="ml-auto" role="presentation">
+                    <button class="inline-block px-4 pt-4 pb-1 border-b-2 rounded-t-lg" id="product-tab"
+                        data-tabs-target="#product" type="button" role="tab" aria-controls="product"
+                        aria-selected="false">Product</button>
+                </li>
+                <!-- Tab  Category-->
+                <li class="md:mx-52 lg:mx-64" role="presentation">
+                    <button
+                        class="inline-block px-4 pt-4 pb-1 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        id="category-tab" data-tabs-target="#category" type="button" role="tab"
+                        aria-controls="category" aria-selected="false">Category</button>
+                </li>
+                <!-- Tab Carousel -->
+                <li class="mr-auto" role="presentation">
+                    <button
+                        class="inline-block px-4 pt-4 pb-1 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        id="carousel-tab" data-tabs-target="#carousel" type="button" role="tab"
+                        aria-controls="carousel" aria-selected="false">Carousel</button>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Tab Content (showing the content) -->
+        <div id="default-tab-content">
+            <!-- Product Content -->
+            <div class="hidden px-10 pt-2 rounded-lg h-[80vh]" id="product" role="tabpanel" aria-labelledby="product-tab">
+                <h1 class="text-black font-semibold text-xl">Edit Product Detail</h1>
+                <div class="w-full h-full flex flex-row">
+                    <!-- Kolom 2/5 (image)-->
+                    <div class="w-2/6 h-[90%] bg-red-300">
+                        <div class="w-full h-full bg-red-900">
+                            <div class="flex flex-col space-y-4">
+                                <div class="w-full h-96">
+                                    <img id="mainImage" src="{{ asset('img/assets/example/test_shirt.jpg') }}" class="w-full h-full object-cover rounded-lg"
+                                        alt="Main Image">
+                                </div>
+                                <div class="flex space-x-2">
+                                    <img onclick="" src="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Kolom 3/5 (form create product) -->
+                    <div class="w-4/6 h-[90%] bg-pink-300">
+                        <div class="w-full h-full bg-[#FFFCFC] rounded-xl ml-5 p-5">
+                            <!-- header create product -->
+                            <h1 class="text-lg font-semibold">Create Product</h1>
+                            <!-- start of form -->
+                            <form action="" class="w-full mt-3">
+                                <!-- Nama Produk -->
+                                <div class="flex items-center mb-4">
+                                    <!-- Label -->
+                                    <div class="w-3/12">
+                                        <label for="nama-produk" class="text-black font-semibold">Nama Produk</label>
+                                    </div>
+                                    <!-- Input Field -->
+                                    <div class="w-8/12">
+                                        <input type="text" id="nama-produk"
+                                            class="w-full h-8 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
+                                    </div>
+                                    <!-- Icon -->
+                                    <div class="w-1/12 flex justify-end">
+                                        <img src="{{ asset('img/assets/icon/icon_admin_product_edit.svg') }}"
+                                            alt="edit icon" class="w-6 h-6">
+                                    </div>
+                                </div>
+
+                                <!-- Harga Produk -->
+                                <div class="flex items-center mb-4">
+                                    <!-- label -->
+                                    <div class="w-3/12">
+                                        <label for="harga-produk" class="text-black font-semibold">Harga Produk</label>
+                                    </div>
+                                    <!-- input field -->
+                                    <div class="w-8/12">
+                                        <input type="number" id="harga-produk"
+                                            class="w-full h-8 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
+                                    </div>
+                                    <!-- icon -->
+                                    <div class="w-1/12 flex justify-end">
+                                        <img src="{{ asset('img/assets/icon/icon_admin_product_edit.svg') }}"
+                                            alt="edit icon" class="w-6 h-6">
+                                    </div>
+                                </div>
+
+                                <!-- Category -->
+                                <div class="flex items-center mb-4">
+                                    <!-- label -->
+                                    <div class="w-3/12">
+                                        <label for="category" class="text-black font-semibold">Category</label>
+                                    </div>
+                                    <!-- input filed -->
+                                    <div class="w-8/12">
+                                        <select id="dropdown_add_category"
+                                            class="w-full h-8 rounded-lg px-4 text-xs border-black focus:border-black focus:outline-none focus:ring-0">
+                                            <option value="select category">Select Category</option>
+                                            <option value="fashion">Fashion</option>
+                                            <!-- !!! tambahin -->
+                                        </select>
+                                    </div>
+                                    <!-- icon (kosong) -->
+                                    <div class="w-1/12">
+                                        <!-- Kosong untuk category -->
+                                    </div>
+                                </div>
+
+                                <!-- Product Specification -->
+                                <div class="flex items-center mb-4">
+                                    <!-- label -->
+                                    <div class="w-3/12">
+                                        <label for="product-specification" class="text-black font-semibold">Product
+                                            Specification</label>
+                                    </div>
+                                    <!-- input field -->
+                                    <div class="w-8/12">
+                                        <textarea id="product-specification" rows="4"
+                                            class="w-full rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0"></textarea>
+                                    </div>
+                                    <!-- icon -->
+                                    <div class="w-1/12 flex justify-end">
+                                        <img src="{{ asset('img/assets/icon/icon_admin_product_edit.svg') }}"
+                                            alt="edit icon" class="w-6 h-6">
+                                    </div>
+                                </div>
+
+                                <!-- Product Description -->
+                                <div class="flex items-center">
+                                    <div class="w-3/12">
+                                        <label for="product-description" class="text-black font-semibold">Product
+                                            Description</label>
+                                    </div>
+                                    <div class="w-8/12">
+                                        <textarea id="product-description" rows="4"
+                                            class="w-full rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0 text-md"></textarea>
+                                    </div>
+                                    <div class="w-1/12 flex justify-end">
+                                        <img src="{{ asset('img/assets/icon/icon_admin_product_edit.svg') }}"
+                                            alt="edit icon" class="w-6 h-6">
+                                    </div>
+                                </div>
+
+                                <!-- Button Save -->
+                                <div class="flex mt-2 w-full">
+                                    <button type="submit"
+                                        class="bg-orange-400 hover:bg-orange-500 text-white font-semibold w-1/3 h-10 rounded-lg ml-auto mr-14">
+                                        Save
+                                    </button>
+                                </div>
+                            </form>
+                            <!-- end of form -->
+                        </div>
+                    </div>
+                    <!-- end of kolom 2/3 (form product) -->
+                </div>
+            </div>
+
+            <!-- end of product Content -->
+
+            <!-- Category Content -->
+            @include('admin.product.categoryContent')
+            <!-- End of Category Content -->
+
+
+            <!-- Carousel Content -->
+            @include('admin.product.carouselContent')
+            <!-- End of Carousel Content -->
+        </div>
+        <!-- end of tab content -->
+    </div>
+
+    <script>
+        function changeImage(imageSrc){
+            document.getElementById('mainImage').src = imageSrc;
+        }
+    </script>
+@endsection
