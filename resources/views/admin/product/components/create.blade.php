a60b1321f237fe4fb7b5d37060dd772b46426017
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-11-05 13:05:06 +0700

    [fix] edit and create product input & button placement

--- resources/views/admin/product/components/create.blade.php
+++ resources/views/admin/product/components/create.blade.php
@@ -46,7 +46,7 @@
                                         alt="Main Image">
                                 </div>
                                 <div class="flex space-x-2 mx-auto overflow-x-auto" id="product-image-preview"> </div>
-                                <div>
+                                <div class="mt-52">
                                     <button id="btn-add-image"
                                         class="w-3/5 flex items-center justify-center bg-orange-400 h-10 rounded-xl mb-5 text-lg font-bold text-white mt-5S mx-auto">
                                         <img src="{{ asset('img/assets/icon/icon_admin_category_upload.svg') }}"
@@ -68,7 +68,7 @@
                                 enctype="multipart/form-data" id="form-create-product" enctype="multipart/form-data">
                                 @csrf
                                 <!-- Nama Produk -->
-                                <div class="flex items-center mb-4">
+                                <div class="flex items-center mb-10">
                                     <!-- Label -->
                                     <div class="w-3/12">
                                         <label for="nama-produk" class="text-black font-semibold">Nama Produk</label>
@@ -76,7 +76,7 @@
                                     <!-- Input Field -->
                                     <div class="w-8/12">
                                         <input type="text" id="nama-produk" name="name"
-                                            class="w-full h-8 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
+                                            class="w-full h-10 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
                                     </div>
                                     <!-- Icon -->
                                     <div class="w-1/12 flex justify-end">
@@ -86,7 +86,7 @@
                                 </div>
 
                                 <!-- Harga Produk -->
-                                <div class="flex items-center mb-4">
+                                <div class="flex items-center mb-10">
                                     <!-- label -->
                                     <div class="w-3/12">
                                         <label for="harga-produk" class="text-black font-semibold">Harga Produk</label>
@@ -94,7 +94,7 @@
                                     <!-- input field -->
                                     <div class="w-8/12">
                                         <input type="number" id="harga-produk" name="price"
-                                            class="w-full h-8 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
+                                            class="w-full h-10 rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0">
                                     </div>
                                     <!-- icon -->
                                     <div class="w-1/12 flex justify-end">
@@ -104,7 +104,7 @@
                                 </div>
 
                                 <!-- Category -->
-                                <div class="flex items-center mb-4">
+                                <div class="flex items-center mb-10">
                                     <!-- label -->
                                     <div class="w-3/12">
                                         <label for="category" class="text-black font-semibold">Category</label>
@@ -112,7 +112,7 @@
                                     <!-- input filed -->
                                     <div class="w-8/12">
                                         <select id="dropdown_add_category" name="category"
-                                            class="w-full h-8 rounded-lg px-4 text-xs border-black focus:border-black focus:outline-none focus:ring-0">
+                                            class="w-full h-10 rounded-lg px-4 text-xs border-black focus:border-black focus:outline-none focus:ring-0">
                                             @foreach (\App\Models\Category::all() as $category)
                                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                                             @endforeach
@@ -127,12 +127,12 @@
 
                                 <!-- Product Description -->
                                 <div class="flex items-center">
-                                    <div class="w-3/12">
+                                    <div class="w-3/12 mb-auto">
                                         <label for="product-description" class="text-black font-semibold">Product
                                             Description</label>
                                     </div>
                                     <div class="w-8/12">
-                                        <textarea id="product-description" rows="4" name="description" form="form-create-product"
+                                        <textarea id="product-description" rows="5" name="description" form="form-create-product"
                                             class="w-full rounded-lg px-4 border-black focus:border-black focus:outline-none focus:ring-0 text-md"></textarea>
                                     </div>
                                     <div class="w-1/12 flex justify-end">
@@ -141,16 +141,16 @@
                                     </div>
                                 </div>
 
-                                <div class="flex mt-2 w-full">
+                                <div class="flex mt-4 w-full">
                                     <!-- Button Cancel -->
                                     <button type="button"
                                         onclick="window.location.href='{{ route('admin.product.index') }}'"
-                                        class="bg-red-400 hover:bg-red-500 text-white font-semibold w-1/3 h-10 rounded-lg ml-auto mr-14">
+                                        class="bg-red-400 hover:bg-red-500 text-white font-semibold w-1/3 h-10 rounded-lg ml-auto mr-2">
                                         Cancel
                                     </button>
                                     <!-- Button Save -->
                                     <button type="submit"
-                                        class="bg-orange-400 hover:bg-orange-500 text-white font-semibold w-1/3 h-10 rounded-lg ml-auto mr-14">
+                                        class="bg-orange-400 hover:bg-orange-500 text-white font-semibold w-1/3 h-10 rounded-lg mr-16">
                                         Save
                                     </button>
                                 </div>
