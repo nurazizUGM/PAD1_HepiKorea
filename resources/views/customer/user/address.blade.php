84e3de9b3b6ba37ab8312bdfc92912f253132a23
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-11-14 20:51:41 +0700

    address fixed

--- resources/views/customer/user/address.blade.php
+++ resources/views/customer/user/address.blade.php
@@ -49,35 +49,35 @@
                                 data-modal-target="address-edit-modal" data-modal-toggle="address-edit-modal">Edit</button>
                             <button
                                 class="w-fit bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-lg font-semibold rounded-2xl py-2 px-7 ml-2"
-                                data-modal-target="change-address-modal"
-                                data-modal-toggle="change-address-modal">Delete</button>
+                                data-modal-target="confirmation-delete-modal"
+                                data-modal-toggle="confirmation-delete-modal">Delete</button>
                         </div>
                     </div>
                 @endfor
 
             </div>
             <button
-                class="w-fit flex flex-row bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-lg font-semibold rounded-2xl justify-center items-center py-2 pl-10 pr-12 mr-auto mt-auto"
-                data-modal-target="address-edit-modal" data-modal-toggle="address-edit-modal">
+                class="w-fit flex flex-row bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-lg font-semibold rounded-2xl justify-center items-center py-2 pl-10 pr-12 mr-auto mt-auto">
                 <img src="{{ asset('img/assets/icon/icon_arrow_back.svg') }}" alt="" class="w-10 h-8">
                 <p class="my-auto">Back</p>
             </button>
 
             {{-- Address --}}
 
+            {{-- MODALS FOR ADDRES --}}
 
             <!-- Pop Up Add Address-->
             <div id="address-add-modal" tabindex="-1" aria-hidden="true"
                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-[999999] justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative p-[0.1rem] w-fit max-w-4xl max-h-full">
                     <!-- Modal content -->
-                    <div class="bg-white w-[55vw] rounded-lg shadow overflow-y-auto px-10 py-5">
+                    <div class="bg-white w-[55vw] rounded-[30px] shadow overflow-y-auto px-10 py-5">
                         <div class="w-full h-full flex flex-col">
                             <h1 class="text-black font-bold text-2xl p-5 pb-2 ml-6">New Address</h1>
                             <button type="button"
-                                class="absolute bg-black w-5 h-5 flex flex-col align-middle text-center items-center rounded-full pb-3 -top-2 -right-2"
+                                class="absolute bg-black w-6 h-6 flex flex-col align-middle text-center items-center rounded-full pb-3 top-0 right-0"
                                 data-modal-hide="address-add-modal">
-                                <p class="m-auto text-white text-sm">X</p>
+                                <p class="m-auto text-white text-md">X</p>
                             </button>
                             <div class="w-full h-full flex flex-col">
                                 <form action="" method="" class="flex flex-col h-full text-center py-2 px-5">
@@ -144,9 +144,11 @@
                                         </tbody>
                                     </table>
                                     <button type="submit" data-modal-hide="address-add-modal"
-                                        class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-base font-semibold mx-auto mt-3 mb-3 inline-block w-1/4 h-10 rounded-xl">
+                                        class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-base font-semibold mx-auto mt-3 mb-3 inline-block w-1/4 h-10 rounded-xl"
+                                        data-modal-target="success-added-modal" data-modal-toggle="success-added-modal">
                                         Save
                                     </button>
+                                    {{-- success-added-modal --}}
                                 </form>
                             </div>
                         </div>
@@ -161,13 +163,13 @@
                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative p-[0.1rem] w-fit max-w-4xl max-h-full">
                     <!-- Modal content -->
-                    <div class="bg-white w-[55vw] rounded-lg shadow overflow-y-auto px-10 py-5">
+                    <div class="bg-white w-[55vw] rounded-[30px] shadow overflow-y-auto px-10 py-5">
                         <div class="w-full h-full flex flex-col">
                             <h1 class="text-black font-bold text-2xl p-5 pb-2 ml-6">Edit Address</h1>
                             <button type="button"
-                                class="absolute bg-black w-5 h-5 flex flex-col align-middle text-center items-center rounded-full pb-3 -top-1 -right-6"
+                                class="absolute bg-black w-6 h-6 flex flex-col align-middle text-center items-center rounded-full pb-3 top-0 right-0"
                                 data-modal-hide="address-edit-modal">
-                                <p class="m-auto text-white text-sm">X</p>
+                                <p class="m-auto text-white text-md">X</p>
                             </button>
                             <div class="w-full h-full flex flex-col">
                                 <form action="" method="" class="flex flex-col h-full text-center py-2 px-5">
@@ -234,7 +236,8 @@
                                         </tbody>
                                     </table>
                                     <button type="submit" data-modal-hide="address-edit-modal"
-                                        class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-base font-semibold mx-auto mt-3 mb-3 inline-block w-1/4 h-10 rounded-xl">
+                                        class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] text-white text-base font-semibold mx-auto mt-3 mb-3 inline-block w-1/4 h-10 rounded-xl"
+                                        data-modal-target="success-updated-modal" data-modal-toggle="success-updated-modal">
                                         Save
                                     </button>
                                 </form>
@@ -245,6 +248,105 @@
                 </div>
             </div>
             <!-- end of Address-edit-modal -->
+
+
+            {{-- delete confirmation modal --}}
+            <div id="confirmation-delete-modal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-fit max-w-5xl max-h-full mt-20">
+                    <!-- Modal content -->
+                    <div class="bg-white w-[33vw] h-auto rounded-[30px] shadow">
+                        <div class="relative w-full h-full flex flex-row">
+                            <div class="w-full h-full flex flex-col px-10 py-10">
+                                <img src="{{ asset('img/assets/icon/icon_warning.svg') }}" alt="icon_warning"
+                                    class="w-16 h-16 mx-auto">
+                                <p class="text-[#376F7E] font-medium text-xl mx-auto mt-2">Are you sure?</p>
+                                <p class="text-[#B7B7B7] font-medium text-xs mx-auto mt-6">You won’t be able to revert
+                                    this!</p>
+                                <div class="w-full h-full mt-6 flex flex-row justify-center">
+                                    <button
+                                        class="w-44 h-11 bg-[#376F7E] rounded-[20px] shadow-lg text-white text-lg font-semibold"
+                                        data-modal-hide="confirmation-delete-modal"
+                                        data-modal-target="success-delete-modal"
+                                        data-modal-toggle="success-delete-modal">Yes, Delete it!</button>
+                                    {{-- success-delete-modal --}}
+                                    <button
+                                        class="w-44 h-11 bg-[#FF9D66] rounded-[20px] shadow-lg text-white text-lg font-semibold ml-2"
+                                        data-modal-hide="confirmation-delete-modal">Cancel</button>
+                                </div>
+                            </div>
+                            {{-- end of modal content --}}
+                        </div>
+                    </div>
+                    <!-- end of modal content -->
+                </div>
+            </div>
+            {{-- end of delete confirmation modal --}}
+
+
+            {{-- success delete modal --}}
+            <div id="success-delete-modal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-fit max-w-5xl max-h-full mt-20">
+                    <!-- Modal content -->
+                    <div class="bg-white w-[25vw] h-auto rounded-[30px] shadow">
+                        <div class="relative w-full h-full flex flex-row">
+                            <div class="w-full h-full flex flex-col p-14">
+                                <h1 class="text-black text-xl font-medium mx-auto">Successfully Deleted!</h1>
+                                <img src="{{ asset('img/assets/icon/icon_green_check.svg') }}" alt="green_check"
+                                    class="w-24 h-24 mx-auto mt-6">
+                            </div>
+                            {{-- end of modal content --}}
+                        </div>
+                    </div>
+                    <!-- end of modal content -->
+                </div>
+            </div>
+            {{-- end of success delete modal --}}
+
+
+            {{-- success added modal --}}
+            <div id="success-added-modal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-fit max-w-5xl max-h-full mt-20">
+                    <!-- Modal content -->
+                    <div class="bg-white w-[25vw] h-auto rounded-[30px] shadow">
+                        <div class="relative w-full h-full flex flex-row">
+                            <div class="w-full h-full flex flex-col p-14">
+                                <h1 class="text-black text-xl font-medium mx-auto">Successfully Added!</h1>
+                                <img src="{{ asset('img/assets/icon/icon_green_check.svg') }}" alt="green_check"
+                                    class="w-24 h-24 mx-auto mt-6">
+                            </div>
+                            {{-- end of modal content --}}
+                        </div>
+                    </div>
+                    <!-- end of modal content -->
+                </div>
+            </div>
+            {{-- end of success delete modal --}}
+            
+            
+            {{-- success edited modal --}}
+            <div id="success-updated-modal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-fit max-w-5xl max-h-full mt-20">
+                    <!-- Modal content -->
+                    <div class="bg-white w-[25vw] h-auto rounded-[30px] shadow">
+                        <div class="relative w-full h-full flex flex-row">
+                            <div class="w-full h-full flex flex-col p-14">
+                                <h1 class="text-black text-xl font-medium mx-auto">Successfully Updated!</h1>
+                                <img src="{{ asset('img/assets/icon/icon_green_check.svg') }}" alt="green_check"
+                                    class="w-24 h-24 mx-auto mt-6">
+                            </div>
+                            {{-- end of modal content --}}
+                        </div>
+                    </div>
+                    <!-- end of modal content -->
+                </div>
+            </div>
+            {{-- end of success delete modal --}}
+
+            {{-- end of MODALS FOR ADDRESS --}}
         </div>
     </div>
 
