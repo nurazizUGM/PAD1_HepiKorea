73d8f2be25e4f084be9d2bf71f6ea8f872f5658c
Nur Aziz <sukabaru09@gmail.com>
2024-11-16 15:41:51 +0700

    [customer] update noptification layout

--- resources/views/customer/notification.blade.php
+++ resources/views/customer/notification.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.customer_nofooter_scrollable')
+@extends('layout.customer')
 @section('title', 'Notification')
 
 @section('content')
@@ -8,26 +8,28 @@
             <h1 class="text-black font-semibold text-2xl">Notification</h1>
             {{-- list of notification container --}}
             <div class="w-full h-full flex flex-col gap-y-4 mt-4">
-                
+
                 @for ($i = 0; $i < 10; $i++)
-                {{-- Notification --}}
-                <div class="w-full h-fit bg-white rounded-2xl shadow-lg p-4">
-                    {{-- notification date --}}
-                    <p class="text-[#B7B7B7] font-semibold text-sm">27-Sep-2024</p>
-                    <div class="w-ful h-full flex flex-row mt-3">
-                        {{-- image product --}}
-                        <img src="{{ asset('img/example/admin_order_img_phone.png') }}" alt="img_product" class="h-20 object-contain">
-                        <div class="flex flex-col ml-10">
-                            {{-- product name --}}
-                            <h1 class="text-[#3E6E7A] text-base font-semibold">Samsung Ultra S24</h1>
-                            {{-- product variant --}}
-                            <p class="text-[#C4C4C4] text-xs font-semibold mt-1">Black</p>
+                    {{-- Notification --}}
+                    <div class="w-full h-fit bg-white rounded-2xl shadow-lg p-4">
+                        {{-- notification date --}}
+                        <p class="text-[#B7B7B7] font-semibold text-sm">27-Sep-2024</p>
+                        <div class="w-ful h-full flex flex-row mt-3">
+                            {{-- image product --}}
+                            <img src="{{ asset('img/example/admin_order_img_phone.png') }}" alt="img_product"
+                                class="h-20 object-contain">
+                            <div class="flex flex-col ml-10">
+                                {{-- product name --}}
+                                <h1 class="text-[#3E6E7A] text-base font-semibold">Samsung Ultra S24</h1>
+                                {{-- product variant --}}
+                                <p class="text-[#C4C4C4] text-xs font-semibold mt-1">Black</p>
+                            </div>
+                            {{-- Status of product --}}
+                            <h1 class="text-[#3E6E7A] text-xl font-semibold my-auto ml-auto">Your product has been
+                                confirmed!</h1>
                         </div>
-                        {{-- Status of product --}}
-                        <h1 class="text-[#3E6E7A] text-xl font-semibold my-auto ml-auto">Your product has been confirmed!</h1>
                     </div>
-                </div>
-                {{-- end of Notification --}}
+                    {{-- end of Notification --}}
                 @endfor
 
             </div>
@@ -37,3 +39,6 @@
     </div>
     <div class="h-10 w-full"></div>
 @endsection
+
+{{-- disable footer --}}
+@section('footer', '')
