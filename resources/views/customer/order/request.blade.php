6d79442d7994723e72bd6871a4c4fba8d1fef219
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-11-29 09:00:04 +0700

    [customer request order] responsive

--- resources/views/customer/order/request.blade.php
+++ resources/views/customer/order/request.blade.php
@@ -2,21 +2,21 @@
 @section('title', 'Request Order')
 
 <a href=""
-    class="fixed flex flex-row bottom-8 right-8 px-2 py-2 text-2xl font-bold text-[#3E6E7A] bg-white rounded-3xl align-middle items-center shadow-md hover:shadow-lg z-30">
-    <img src="{{ asset('img/assets/icon/icon_customer_chat.svg') }}" alt="icon_Chat" class="w-10 h-10 mr-1">
+    class="fixed flex flex-row bottom-8 right-8 px-2 py-2 text-base md:text-2xl font-bold text-[#3E6E7A] bg-white rounded-3xl align-middle items-center shadow-md hover:shadow-lg z-30">
+    <img src="{{ asset('img/assets/icon/icon_customer_chat.svg') }}" alt="icon_Chat" class="w-5 h-5 md:w-10 md:h-10 mr-1">
     <p>Chat</p>
 </a>
 
 @section('content')
-    <div class="w-full w-max[100%] h-full rounded-3xl bg-[#EFEFEF] shadow-md overflow-hidden py-6 px-6">
+    <div class="w-full w-max[100%] h-full rounded-3xl bg-[#EFEFEF] shadow-md overflow-hidden py-3 md:py-6 px-3 md:px-6">
         {{-- start of welcome container --}}
-        <div class="bg-white w-full h-full flex flex-col rounded-xl pt-4 pb-8 px-8">
-            <h1 class="text-2xl font-bold">Welcome !</h1>
-            <p class="font-semibold text-lg">
+        <div class="bg-white w-full h-full flex flex-col rounded-xl pt-2 md:pt-4 pb-4 md:pb-8 px-4 md:px-8">
+            <h1 class="text-lg md:text-2xl font-bold">Welcome !</h1>
+            <p class="font-semibold text-xs md:text-lg">
                 Once you've selected your items from your preferred Korean store, complete this form to place your custom
                 order and <br> let us handle the rest.
             </p>
-            <p class="font-semibold text-lg mt-8">
+            <p class="font-semibold text-xs md:text-lg mt-2 md:mt-8">
                 Not sure where to look beyond HepiKorea? Feel free to use our search engine to explore other Korean stores.
                 Say <br> goodbye to hopping between websites!
             </p>
@@ -24,54 +24,54 @@
         {{-- end of welcome container --}}
 
         {{-- start of step pemesanan & form customer details --}}
-        <div class="w-full h-full bg-white flex-col rounded-xl mt-6 py-6 px-8">
-            <h1 class="text-center text-4xl text-black font-bold mb-5">Step of Request Order</h1>
+        <div class="w-full h-full bg-white flex-col rounded-xl mt-6 py-2 md:py-6 px-2 md:px-8">
+            <h1 class="text-center text-xl md:text-4xl text-black font-bold mb-1 md:mb-5">Step of Request Order</h1>
 
             {{-- container card step pemesanan --}}
             <div class="w-full h-full flex flex-col">
                 <div class="flex flex-row h-1/2">
-                    <div class="w-1/2 h-fit flex p-2">
+                    <div class="w-1/2 h-full flex p-2">
                         {{-- step number 1 --}}
                         <div
-                            class="w-fit h-fit bg-white rounded-xl mr-auto shadow-lg hover:shadow-xl my-auto flex py-10 pl-5 pr-16">
-                            <div class="text-[#3E6E7A] text-lg font-semibold m-auto flex flex-row">
-                                <h1 class="text-[96px] font-bold">1</h1>
-                                <h1 class="ml-2">Complete Customer Detail</h1>
+                            class="w-fit h-fit bg-white rounded-xl mr-auto shadow-lg hover:shadow-xl my-auto flex py-2 md:py-10 pl-2.5 md:pl-5 pr-2.5 md:pr-16">
+                            <div class="text-[#3E6E7A] text-[8px] md:text-lg font-semibold m-auto flex flex-row">
+                                <h1 class="text-[38px] md:text-[96px] font-bold">1</h1>
+                                <h1 class="ml-2 my-auto">Complete Customer Detail</h1>
                             </div>
                         </div>
                         {{-- end of step number 1 --}}
                     </div>
                     <div class="w-1/2 h-full flex p-2">
-                        {{-- step number 2 --}}
-                        <div
-                            class="w-fit h-fit bg-[#3E6E7A] rounded-xl mr-auto shadow-lg hover:shadow-xl my-auto flex py-10 pl-5 pr-20">
-                            <div class="text-white text-lg font-semibold m-auto flex flex-row">
-                                <h1 class="text-[96px] font-bold">3</h1>
-                                <h1 class="ml-2">Confirm the product</h1>
-                            </div>
-                        </div>
-                        {{-- end of step number 2 --}}
-                    </div>
-                </div>
-                <div class="flex flex-row h-1/2 pt-4">
-                    <div class="w-1/2 h-full flex p-2">
                         {{-- step number 3 --}}
                         <div
-                            class="w-fit h-fit bg-[#3E6E7A] rounded-xl ml-auto shadow-lg hover:shadow-xl my-auto flex py-10 pl-5 pr-10">
-                            <div class="text-white text-lg font-semibold m-auto flex flex-row">
-                                <h1 class="text-[96px] font-bold">2</h1>
-                                <h1 class="ml-2">Write the detail of product request</h1>
+                            class="w-fit h-fit bg-[#3E6E7A] rounded-xl mr-auto shadow-lg hover:shadow-xl my-auto flex py-2 md:py-10 pl-2.5 md:pl-5 pr-2.5 md:pr-20">
+                            <div class="text-white text-[8px] md:text-lg font-semibold m-auto flex flex-row">
+                                <h1 class="text-[38px] md:text-[96px] font-bold">3</h1>
+                                <h1 class="ml-2 my-auto">Confirm the product</h1>
                             </div>
                         </div>
                         {{-- end of step number 3 --}}
                     </div>
+                </div>
+                <div class="flex flex-row h-1/2 pt-4">
+                    <div class="w-1/2 h-full flex p-2">
+                        {{-- step number 2 --}}
+                        <div
+                            class="w-fit h-fit bg-[#3E6E7A] rounded-xl ml-auto shadow-lg hover:shadow-xl my-auto flex py-2 md:py-10 pl-2.5 md:pl-5 pr-2.5 md:pr-10">
+                            <div class="text-white text-[8px] md:text-lg font-semibold m-auto flex flex-row">
+                                <h1 class="text-[38px] md:text-[96px] font-bold">2</h1>
+                                <h1 class="ml-2 my-auto">Write the detail of product request</h1>
+                            </div>
+                        </div>
+                        {{-- end of step number 2 --}}
+                    </div>
                     <div class="w-1/2 h-full flex p-2">
                         {{-- step number 4 --}}
                         <div
-                            class="w-fit h-fit bg-white rounded-xl ml-auto shadow-lg hover:shadow-xl my-auto flex py-10 pl-5 pr-6">
-                            <div class="text-[#3E6E7A] text-lg font-semibold m-auto flex flex-row">
-                                <h1 class="text-[96px] font-bold">4</h1>
-                                <h1 class="ml-2">Wait the product confirmed</h1>
+                            class="w-fit h-fit bg-white rounded-xl ml-auto shadow-lg hover:shadow-xl my-auto flex py-2 md:py-10 pl-2.5 md:pl-5 pr-2.5 md:pr-6">
+                            <div class="text-[#3E6E7A] text-[8px] md:text-lg font-semibold m-auto flex flex-row">
+                                <h1 class="text-[38px] md:text-[96px] font-bold">4</h1>
+                                <h1 class="ml-2 my-auto">Wait the product confirmed</h1>
                             </div>
                         </div>
                         {{-- end of step number 4 --}}
@@ -80,17 +80,17 @@
             </div>
 
             {{-- form Customer details --}}
-            <form id="form-request" action="{{ route('request-order') }}" class="w-full h-full flex flex-col mt-10"
+            <form id="form-request" action="{{ route('request-order') }}" class="w-full h-full flex flex-col mt-2 md:mt-10"
                 method="POST" enctype="multipart/form-data">
                 @csrf
-                <label for="name" class="text-xl text-black font-medium my-2">Name</label>
+                <label for="name" class="text-base md:text-xl text-black font-medium my-0.5 md:my-2">Name</label>
                 <input type="text" name="fullname"
-                    class="w-[55%] h-12 pl-5 bg-white outline outline-1 border-black rounded-xl my-2 border-0 focus:border-1 focus:border-black focus:ring-black"
+                    class="w-full md:w-[55%] h-8 md:h-12 pl-5 bg-white outline outline-1 border-black rounded-lg md:rounded-xl my-0.5 md:my-2 border-0 focus:border-1 focus:border-black focus:ring-black"
                     placeholder="Enter your name">
-                <label for="email" class="text-xl text-black font-medium my-2">Email</label>
+                <label for="email" class="text-base md:text-xl text-black font-medium my-0.5 md:my-2">Email</label>
                 <div class="my-2 flex flex-row">
                     <input type="email" name="email"
-                        class="w-[55%] h-12 pl-5 bg-white outline outline-1 border-black rounded-xl py-2 border-0 focus:border-1 focus:border-black focus:ring-black"
+                        class="w-full md:w-[55%] h-8 md:h-12 pl-5 bg-white outline outline-1 border-black rounded-lg md:rounded-xl py-2 border-0 focus:border-1 focus:border-black focus:ring-black"
                         placeholder="Email address">
                 </div>
             </form>
@@ -105,14 +105,14 @@
 
                 {{-- end of request order product card --}}
             @endfor
-            <a href="#" class="ml-auto w-1/12"><button type="submit" form="form-request"
-                    class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] rounded-lg text-white text-lg py-2 px-5 ">Confirm</button></a>
+            <a href="#" class="ml-auto w-1/4 md:w-1/12"><button type="submit" form="form-request"
+                    class="bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] rounded-lg text-white text-sm md:text-lg py-1.5 md:py-2 px-2 md:px-5 ">Confirm</button></a>
         </div>
         {{-- end container list request order --}}
 
         {{-- start of request order form --}}
-        <div class="w-full h-full bg-white flex-col rounded-xl mt-6 py-6 px-8">
-            <form action="" class="w-full h-fit flex flex-col text-orange-400 font-semibold text-xl">
+        <div class="w-full h-full bg-white flex-col rounded-xl mt-6 py-3 md:py-6 px-4 md:px-8">
+            <form action="" class="w-full h-fit flex flex-col text-orange-400 font-semibold text-base md:text-xl">
                 <label for="">Product Name</label>
                 {{-- input product name --}}
                 <input type="text" id="product-name" name="product-name"
@@ -152,7 +152,7 @@
                     class="rounded-xl bg-white border text-[#898383] font-normal border-black px-6 focus:border-black focus:ring-0 resize-none my-2"></textarea>
 
                 <button type="button" onclick="addOrderItem()"
-                    class="text-white bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] w-1/12 py-2 rounded-lg font-normal mt-0.5 mr-auto">Add</button>
+                    class="text-white bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] w-1/5 md:w-1/12 py-1 md:py-2 rounded-lg font-normal mt-0.5 mr-auto">Add</button>
             </form>
         </div>
         {{-- end of request order form --}}
@@ -248,18 +248,18 @@
             });
 
             const component = $(`
-            <div class="w-full h-fit bg-white rounded-xl flex flex-row px-4 py-4 mb-4 request-order-items">
-                <div class="w-[15%] h-[10rem] bg-contain bg-no-repeat bg-center"
+            <div class="w-full h-fit bg-white rounded-xl flex flex-row px-1 md:px-4 py-2 md:py-4 mb-4 request-order-items">
+                <div class="w-[15%] h-[6rem] md:h-[10rem] bg-contain bg-no-repeat bg-center"
                     style="background-image: url('${productImagePreview}')"></div>
                 <div class="w-[42%] pl-5 flex flex-col">
                     <div class="flex flex-row mb-auto align-middle">
-                        <h1 class="text-xl font-medium text-black mr-auto">${productName}</h1>
+                        <h1 class="text-xs md:text-xl font-medium text-black mr-auto">${productName}</h1>
                         <input type="hidden" form="form-request" name="items[${productCount}][name]" value="${productName}">
-                        <h1 class="text-xl font-semibold text-[#B7B7B7] mx-auto">Rp ${productPrice}</h1>
+                        <h1 class="text-xs md:text-xl font-semibold text-[#B7B7B7] mx-auto">Rp ${productPrice}</h1>
                         <input type="hidden" form="form-request" name="items[${productCount}][price]" value="${productPrice}">
                     </div>
                     <div class="w-full h-full flex flex-col mt-auto">
-                        <p class="font-medium text-lg text-black mt-auto">Link:</p>
+                        <p class="font-medium text-xs md:text-lg text-black mt-auto">Link:</p>
                         <textarea form="form-request" name="items[${productCount}][url]" cols="" rows="2" readonly
                             class="rounded-2xl resize-none">${productLink}</textarea>
                     </div>
@@ -267,16 +267,16 @@
                 <div class="w-[43%] pl-5 flex flex-col">
                     <div class="flex flex-row mb-auto align-middle">
                         <input type="hidden" form="form-request" name="items[${productCount}][quantity]" value="${productQuantity}">
-                        <h1 class="text-xl font-medium text-[#B7B7B7] mr-auto">${productQuantity}x</h1>
-                        <h1 class="text-xl font-semibold text-orange-400 mx-auto">Rp ${productPrice*productQuantity}</h1>
+                        <h1 class="text-xs md:text-xl font-medium text-[#B7B7B7] mr-auto">${productQuantity}x</h1>
+                        <h1 class="text-xs md:text-xl font-semibold text-orange-400 mx-auto">Rp ${productPrice*productQuantity}</h1>
                         <button href=""
                             class="bg-orange-400 hover:bg-orange-500 focus:bg-orange-600 p-1 rounded-md"><img
                                 src="{{ asset('img/assets/icon/icon_requestOrder_trashcan.svg') }}" alt=""
-                                class="w-6 h-6" data-modal-target="confirmation-delete-modal"
+                                class="w-3 md:w-6 h-3 md:h-6" data-modal-target="confirmation-delete-modal"
                                 data-modal-toggle="confirmation-delete-modal"></button>
                     </div>
                     <div class="w-full h-full flex flex-col mt-auto">
-                        <p class="font-medium text-lg text-black mt-auto">Note:</p>
+                        <p class="font-medium text-xs md:text-lg text-black mt-auto">Note:</p>
                         <textarea form="form-request" name="items[${productCount}][description]" cols="" rows="2" placeholder="" only
                             class="rounded-2xl resize-none text-[#898383]">${productDescription}</textarea>
                     </div>
