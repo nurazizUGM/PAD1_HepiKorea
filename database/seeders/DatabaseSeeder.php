cbe0b01f2d5a3f0d9086abcbedc524b3dffb0479
Nur Aziz <sukabaru09@gmail.com>
2024-12-04 10:05:20 +0700

    separate DataSeeder from main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SettingSeeder::class,
             AdminSeeder::class,
-            DataSeeder::class,
         ]);
     }
 }
