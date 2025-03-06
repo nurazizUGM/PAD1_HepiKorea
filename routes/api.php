6b58378c2fa64f8a1b3db3d71a8ba1b46a6e739f
Nur Aziz <sukabaru09@gmail.com>
2025-03-07 02:11:35 +0700

    feat: api products - catalog - carousel - category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CarouselController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Middleware\Admin;
 use App\Http\Middleware\ApiAuth;
 use Illuminate\Support\Facades\Route;
 
@@ -36,6 +40,39 @@ Route::name('api.')->group(function () {
             Route::post('profile', 'updateProfile');
             Route::post('verify', 'verify');
             Route::post('verify-otp', 'verifyOtp');
+        });
+    });
+
+    Route::prefix('product')->controller(ProductController::class)->group(function () {
+        Route::get('latest', 'latest');
+        Route::get('popular', 'popular');
+        Route::get('/', 'findAll');
+        Route::get('/{id}', 'findOne');
+
+        Route::middleware([ApiAuth::class, Admin::class])->group(function () {
+            Route::post('/', 'create');
+            Route::post('/{id}', 'update');
+            Route::delete('/{id}', 'delete');
+        });
+    });
+
+    Route::prefix('category')->controller(CategoryController::class)->group(function () {
+        Route::get('/', 'findAll');
+
+        Route::middleware([ApiAuth::class, Admin::class])->group(function () {
+            Route::post('/', 'create');
+            Route::post('/{id}', 'update');
+            Route::delete('/{id}', 'delete');
+        });
+    });
+
+    Route::prefix('carousel')->controller(CarouselController::class)->group(function () {
+        Route::get('/', 'findAll');
+
+        Route::middleware([ApiAuth::class, Admin::class])->group(function () {
+            Route::post('/', 'create');
+            Route::post('/{id}', 'update');
+            Route::delete('/{id}', 'delete');
         });
     });
 });
