87ad2b4cb3e563be0eaaaab3f1730c72438cd529
Nur Aziz <sukabaru09@gmail.com>
2025-03-16 23:31:43 +0700

    feat: set favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/favicon.ico" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @routes
     @inertiaHead
