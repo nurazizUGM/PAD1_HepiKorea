df12287617071ef6188722c90d53bb5373624e39
Nur Aziz <sukabaru09@gmail.com>
2025-03-21 10:40:50 +0700

    fix: store API access token

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,19 +37,16 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = Auth::check() ? User::find(Auth::id()) : null;
+        $accessToken = $user ? $user->createToken('web')->plainTextToken : null;
+
         return array_merge(parent::share($request), [
             'g_client_id' => env('GOOGLE_CLIENT_ID'),
             'app.debug' => config('app.debug'),
             'app.name' => config('app.name'),
             'apiUrl' => env('API_URL', 'http://localhost:8000/api'),
-            'user' => Auth::check() ?
-                User::find(Auth::id())->only(
-                    'fullname',
-                    'email',
-                    'photo',
-                    'role'
-                )
-                : null,
+            'user' => $user ? $user->only('id', 'name', 'email') : null,
+            'accessToken' => $accessToken,
         ]);
     }
 }
