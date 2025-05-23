ecc9e04331a7d7ff084c2554c040160be7d532ef
Nur Aziz <sukabaru09@gmail.com>
2025-05-23 08:54:19 +0700

    fix: admin page layout duplicate

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,16 +3,11 @@ import { ZiggyVue } from "ziggy-js";
 
 import { createInertiaApp } from "@inertiajs/vue3";
 import { createApp, h } from "vue";
-import Admin from "./Pages/Layouts/Admin.vue";
 
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        let page = pages[`./Pages/${name}.vue`];
-        if (!page.default.layout) {
-            page.default.layout = name.startsWith("Admin/") ? Admin : undefined;
-        }
-        return page;
+        return pages[`./Pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
