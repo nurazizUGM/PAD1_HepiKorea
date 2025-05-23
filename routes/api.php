e7a7c5e70af67adfbaab3baf8e20940483acdf7e
Nur Aziz <sukabaru09@gmail.com>
2025-05-23 21:45:24 +0700

    fix: admin order api

--- routes/api.php
+++ routes/api.php
@@ -138,6 +138,9 @@ Route::name('api.')->group(function () {
         Route::get('statistics', [AdminDashboardController::class, 'index']);
         Route::prefix('order')->controller(AdminOrderController::class)->group(function () {
             Route::get('/', 'orders');
+            Route::post('/{id}/process', 'process');
+            Route::post('/{id}/shipment-invoice', 'createShipmentInvoice');
+            Route::post('/{id}/shipment', 'send');
         });
     });
 
