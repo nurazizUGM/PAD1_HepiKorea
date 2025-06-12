533c1d608f49e631fa7450a71e28725af28f7b9f
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-06-12 19:45:10 +0700

    [Customer - Layout] adjust icon layout

--- resources/js/Pages/Layouts/Customer.vue
+++ resources/js/Pages/Layouts/Customer.vue
@@ -144,7 +144,8 @@ onMounted(() => {
                                     <div v-if="showNotifications"
                                         class="w-[13rem] md:w-[30rem] z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 shadow rounded-xl absolute -right-8 md:right-0">
                                         <div class="px-4 py-3">
-                                            <p class="text-[11px] md:text-sm font-medium text-gray-900">Notifications</p>
+                                            <p class="text-[11px] md:text-sm font-medium text-gray-900">Notifications
+                                            </p>
                                         </div>
                                         <ul class="py-1">
                                             <li v-for="notification in notifications" :key="notification.id">
@@ -283,11 +284,11 @@ onMounted(() => {
                         FAQ </Link>
                         <div v-if="isAdmin && isAuthenticated">
                             <Link :href="route('admin.dashboard')"
-                                class="flex flex-row text-lg font-semibold text-[#B7B7B7] hover:bg-gray-100 group">
+                                class="flex flex-row items-center text-xs font-semibold text-[#B7B7B7] hover:bg-gray-100 group">
                             <img src="/img/assets/icon/icon_dashboard_admin.svg" alt=""
                                 class="w-4 h-4 grayscale group-hover:grayscale-0" />
                             <p class="ml-2 mr-6 whitespace-nowrap group-hover:text-orange-400">
-                                Admin Dashboard
+                                Admin <br> Dashboard
                             </p>
                             </Link>
                         </div>
@@ -328,7 +329,7 @@ onMounted(() => {
                             <div class="relative">
                                 <button @click="toggleNotifications" class="relative">
                                     <img src="/img/assets/icon/icon_customer_notification_green.svg" alt=""
-                                        class="w-4 h-5" />
+                                        class="w-4 h-5 mt-1.5" />
                                     <span v-if="notificationCount > 0"
                                         class="absolute top-[-2px] right-[-2px] w-3 h-3 text-xs text-center font-bold leading-none text-white bg-[#FF2E00] rounded-full">{{
                                             notificationCount }}</span>
@@ -411,7 +412,7 @@ onMounted(() => {
                     </div>
                     <Link v-else :href="route('auth.login')"
                         class="hidden md:flex cursor-pointer text-white bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] focus:outline-none focus:ring-0 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2"
-                         id="login-container">
+                        id="login-container">
                     Login
                     </Link>
                 </div>
