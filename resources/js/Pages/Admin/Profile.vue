04a4bf391e835bcf088c360223d5ff823b8d1419
Nur Aziz <sukabaru09@gmail.com>
2025-06-15 00:01:49 +0700

    feat: admin profile

--- resources/js/Pages/Admin/Profile.vue
+++ resources/js/Pages/Admin/Profile.vue
@@ -7,12 +7,12 @@
                 <div class="grid h-full gap-x-4 grid-cols-[2fr_4fr] mt-6">
                     <div class="bg-white h-full flex flex-col rounded-xl mt-4">
                         <div class="rounded-xl bg-slate-300">
-                            <img class="w-full min-h-[10rem] m-0 p-2" id="profile_picture" :src="userProfile"
-                                alt="Profile Picture">
+                            <img class="w-full min-h-[10rem] m-0 p-2" id="profile_picture"
+                                :src="getImageUrl(profile.photo)" alt="Profile Picture">
                         </div>
                         <button
                             class="w-[98%] h-12 mt-4 rounded-3xl bg-[#3E6E7A] hover:bg-[#37626d] active:bg-[#325862] mx-auto p-2"
-                            onclick="$('input[name=photo]').click()">
+                            @click="$refs.photo.click()">
                             <h1 class="text-lg text-white font-semibold">Choose Photo</h1>
                         </button>
                         <button type="button" onclick="window.location.href='{{ route('admin.dashboard') }}'"
@@ -23,8 +23,9 @@
                     </div>
                     <div class="items-start justify-start bg-white h-auto rounded-xl">
                         <div class="relative overflow-x-auto w-full">
-                            <form autocomplete="off">
-                                <input type="file" name="photo" id="photo" class="hidden">
+                            <form autocomplete="off" @submit.prevent="updateProfile">
+                                <input type="file" name="photo" ref="photo" id="photo" @change="changePhoto"
+                                    class="hidden">
                                 <table class="w-full text-sm text-left rtl:text-right">
                                     <tbody>
                                         <tr class="bg-white">
@@ -36,7 +37,6 @@
                                             <td class="px-6 py-4">
                                                 <input type="text" id="fullname" name="fullname"
                                                     v-model="profile.fullname" autocomplete="off"
-                                                    @input="changed = true"
                                                     class="h-12 bg-gray-50 border border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
                                                     placeholder="Your Name" required />
                                             </td>
@@ -49,11 +49,10 @@
                                                     of Birth</label>
                                             </th>
                                             <td class="px-6 py-4 relative">
-                                                <input type="text" id="date_of_birth" name="date_of_birth" datepicker
-                                                    datepicker-format="yyyy-mm-dd" v-model="profile.date_of_birth"
-                                                    autocomplete="off" @change="changed = true"
+                                                <input type="text" id="date_of_birth" name="date_of_birth"
+                                                    autocomplete="off"
                                                     class="h-12 bg-gray-50 border border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
-                                                    placeholder="Your Birth Date" required />
+                                                    placeholder="Your Birth Date" />
                                                 <div
                                                     class="absolute inset-y-0 right-6 pe-6 flex items-center ps-3.5 pointer-events-none">
                                                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -76,7 +75,7 @@
                                             <td class="px-6 py-4 flex" colspan="2">
                                                 <div class="flex items-center mr-5">
                                                     <input id="genderMale" type="radio" name="gender" value="male"
-                                                        @click="profile.gender = 'male'; changed = true"
+                                                        v-model="profile.gender"
                                                         class="w-4 h-4 border-[#376F7E] focus:ring-2 focus:ring-[#376F7E]"
                                                         style="color:#376F7E">
                                                     <label for="genderMale"
@@ -86,7 +85,7 @@
                                                 </div>
                                                 <div class="flex items-center">
                                                     <input id="genderFemale" type="radio" name="gender" value="female"
-                                                        @click="profile.gender = 'male'; changed = true"
+                                                        v-model="profile.gender"
                                                         class="w-4 h-4 border-[#376F7E] focus:ring-2 focus:ring-[#376F7E]"
                                                         style="color:#376F7E">
                                                     <label for="genderFemale"
@@ -105,7 +104,7 @@
                                             </th>
                                             <td class="px-6 py-4">
                                                 <input type="email" id="email" name="email" v-model="profile.email"
-                                                    autocomplete="off" @input="changed = true"
+                                                    autocomplete="off"
                                                     class="h-12 bg-gray-50 border- border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
                                                     placeholder="Your Email" disabled />
                                             </td>
@@ -119,7 +118,7 @@
                                             </th>
                                             <td class="px-6 py-4 relative">
                                                 <input type="password" id="old_password" name="old_password"
-                                                    v-model="old_password" autocomplete="off" @input="changed = true"
+                                                    v-model="old_password" autocomplete="off"
                                                     class="h-12 bg-gray-50 border- border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
                                                     placeholder="Current Password" />
                                                 <span
@@ -135,7 +134,7 @@
                                             </th>
                                             <td class="px-6 py-4 relative">
                                                 <input type="password" id="password" name="password" v-model="password"
-                                                    autocomplete="off" @input="changed = true"
+                                                    autocomplete="off"
                                                     class="h-12 bg-gray-50 border- border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
                                                     placeholder="New Password" />
                                                 <span
@@ -153,7 +152,7 @@
                                             <td class="px-6 py-4 relative">
                                                 <input type="password" id="password_confirmation"
                                                     name="password_confirmation" v-model="password_confirmation"
-                                                    autocomplete="off" @input="changed = true"
+                                                    autocomplete="off"
                                                     class="h-12 bg-gray-50 border- border-[#376F7E] text-[#898383] text-sm rounded-lg focus:ring-0 focus:border-[#376F7E] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-400 dark:focus:border-orange-400"
                                                     placeholder="Confirm Password" />
                                                 <span
@@ -187,8 +186,11 @@
 
 <script lang="ts">
 import { router } from '@inertiajs/vue3';
+import axios from 'axios';
+import { profile } from 'console';
+import { Datepicker } from 'flowbite';
+import moment from 'moment';
 import adminLayout from '../Layouts/Admin.vue';
-import axios from '../../axios';
 
 export default {
     components: {
@@ -196,11 +198,6 @@ export default {
     },
     props: {
         user: Object,
-    },
-    computed: {
-        userProfile() {
-            return this.profile.photo || '/img/assets/icon/icon_user.svg';
-        }
     },
     data() {
         return {
@@ -215,21 +212,117 @@ export default {
             password: '',
             password_confirmation: '',
             show_password: false,
+            photo: null,
             changed: false,
+            datePipcker: null,
+            loading: true
         }
+    },
+    watch: {
+        profile: {
+            handler() {
+                if (!this.loading) {
+                    this.changed = true;
+                }
+            },
+            deep: true,
+        },
+        old_password() {
+            if (!this.loading) {
+                this.changed = true;
+            }
+        },
+        password() {
+            if (!this.loading) {
+                this.changed = true;
+            }
+        },
+        password_confirmation() {
+            if (!this.loading) {
+                this.changed = true;
+            }
+        },
     },
     mounted() {
-        axios.get('/auth/profile').then((response) => {
-            if (response.status === 200) {
-                this.profile = response.data;
-            }
+        this.fetchProfile().then(() => {
+            this.initDatepicker();
         });
     },
-
     methods: {
-        test() {
-            router.visit('/auth/login');
-        }
+        async fetchProfile() {
+            this.loading = true;
+            return axios.get('/api/auth/profile').then((response) => {
+                if (response.status === 200) {
+                    this.profile = response.data;
+                    this.password = '';
+                    this.password_confirmation = '';
+                    this.old_password = '';
+                    setTimeout(() => {
+                        this.loading = false;
+                    }, 1000);
+                }
+            }).catch((error) => {
+                console.error('Error fetching profile data:', error);
+            });
+        },
+        initDatepicker() {
+            const datePickerElement = document.getElementById('date_of_birth');
+            if (datePickerElement) {
+                const datePicker = new Datepicker(datePickerElement, {
+                    format: 'yyyy-mm-dd',
+                    maxDate: moment().format('YYYY-MM-DD'),
+                    autohide: true,
+                });
+                if (this.profile.date_of_birth) {
+                    datePicker.setDate(moment(this.profile.date_of_birth).format('YYYY-MM-DD'));
+                }
+
+                this.datePicker = datePicker;
+            }
+        },
+        updateProfile() {
+            if (this.changed) {
+                const formData = new FormData();
+                formData.append('fullname', this.profile.fullname);
+                formData.append('date_of_birth', moment(this.datePicker.getDate()).format('YYYY-MM-DD'));
+                formData.append('gender', this.profile.gender);
+                if (this.photo) {
+                    formData.append('photo', this.photo);
+                }
+                if (this.password) {
+                    if (this.password !== this.password_confirmation) {
+                        console.error('Password confirmation does not match');
+                        return;
+                    }
+                    formData.append('new_password', this.password);
+                    formData.append('old_password', this.old_password);
+                }
+
+                axios.post('/api/auth/profile', formData).then((response) => {
+                    if (response.status === 200) {
+                        if (this.password) {
+                            router.get('/auth/login')
+                        }
+                        this.changed = false;
+                    }
+                }).catch((error) => {
+                    console.error('Error updating profile:', error);
+                });
+            }
+        },
+        getImageUrl(image) {
+            if (!image) return '/img/assets/icon/icon_user.svg';
+            return /^(http|blob)/.test(image) ? image : `/api/file?path=${encodeURIComponent(image)}`;
+        },
+        changePhoto(event) {
+            const file = event.target.files[0];
+            if (file) {
+                this.profile.photo = URL.createObjectURL(file);
+                this.photo = file;
+                this.changed = true;
+                event.target.value = ''; // Reset the input value
+            }
+        },
     },
 }
 </script>
